8f08e93403f58ffe4d740b57028c815e61fa527d
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-04-10 13:54:34 +0530

    Create box divs in loop for etch-a-sketch

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const container = document.querySelector("#container");
 container.style.display = "flex";
 
+
+
+
+const gridRowContainerDiv = document.createElement("div");
+container.appendChild(gridRowContainerDiv);
+gridRowContainerDiv.style.display = "flex"
+gridRowContainerDiv.style.flexDirection = "column";
+
+
+
+
+
 for(let i = 0; i < 16; i++) {
+
 
 const gridRowDiv = document.createElement("div");
 gridRowDiv.textContent = "divs";
 gridRowDiv.style.border = "5px solid black";
-container.appendChild(gridRowDiv);
+gridRowContainerDiv.appendChild(gridRowDiv);
 
 
 
 
 }
+
