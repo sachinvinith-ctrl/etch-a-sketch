59921eba5d77e26f43112729b98f030b8e2a2008
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-04-21 13:24:09 +0530

    Add button to create a new grid with etch-a-sketch

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let answer;
+
+const button = document.querySelector("button");
+button.addEventListener ("click", () => { answer = 
+
+prompt("Please enter number for which you want to add grids") } )
+
 const container = document.querySelector("#container");
 container.style.display = "flex";
+
 
 for(let i = 0; i < 16; i++) {
 
@@ -7,23 +15,18 @@ const parentDiv = document.createElement("div");
 parentDiv.style.border = "2px solid black";
 container.appendChild(parentDiv);
 
-for(let i = 0; i < 15; i++) {
+for(let i = 0; i < 16; i++) {
 
 const childDiv = document.createElement("div");
-childDiv.style.width = "30px";
-childDiv.style.height = "30px";
+childDiv.style.width = "20px";
+childDiv.style.height = "20px";
 childDiv.style.borderTop = "2px solid black";
 parentDiv.appendChild(childDiv);
 
 childDiv.addEventListener("mouseenter", () => childDiv.style.backgroundColor = "green");
 
-
-
 }
 
 }
-
-
-
 
 
