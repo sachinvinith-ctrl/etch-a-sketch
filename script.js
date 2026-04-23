c68563b725bccd3ab152af29aad8ef15ba35d10c
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-04-23 19:50:50 +0530

    Create default grid and replace that grid if user input

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 
-let answer;
 
+
+const container = document.querySelector("#container");
+container.style.display = "flex";
+
+
+for(let i = 0; i < 16; i++) {
+
+const parentDiv = document.createElement("div");
+parentDiv.style.border = "2px solid black";
+container.appendChild(parentDiv);
+
+
+for(let i = 0; i < 16; i++) {
+
+const childDiv = document.createElement("div");
+childDiv.style.borderTop = "2px solid black";
+childDiv.style.minWidth = "30px";
+childDiv.style.minHeight = "30px";
+parentDiv.appendChild(childDiv);
+
+childDiv.addEventListener("mouseenter", () => childDiv.style.backgroundColor = "green");
+
+}}
+
+let answer;
 
 
 const button = document.querySelector("button");
@@ -10,6 +34,7 @@ prompt("Please enter number for which you want to add grids")
 
 
 const container = document.querySelector("#container");
+container.replaceChildren();
 container.style.display = "flex";
 
 
