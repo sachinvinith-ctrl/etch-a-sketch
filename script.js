81710191971ea60ab87da5f651c7e09ec37b88fe
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-05-03 17:00:55 +0530

    Add new grid based on user input

--- script.js
+++ script.js
@@ -4,16 +4,37 @@ container.style.width = "500px";
 container.style.height = "500px";
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
-container.style.border = "2px dotted black";
+container.style.border = "2px solid black";
 
 
 for(let i = 0; i < 256; i++) {
 
 const childDiv = document.createElement("div");
-childDiv.style.border = "2px solid black";
+childDiv.style.border = "1px solid black";
 childDiv.style.flex = "1";
 childDiv.style.flexBasis = "6.25%"
 container.appendChild(childDiv);
 
+childDiv.addEventListener("mouseenter", () => childDiv.style.backgroundColor = "green");
+
 }
+
+let userInput;
+
+const button = document.querySelector("button");
+button.addEventListener("click", function() { userInput = prompt("Please enter a new div size")
+container.replaceChildren();
+
+for(let i = 0; i < userInput * userInput; i++) {
+
+const childDiv = document.createElement("div");
+childDiv.style.border = "1px solid black";
+childDiv.style.flex = "1";
+childDiv.style.flexBasis = (100 / userInput) + "%";
+container.appendChild(childDiv);
+
+}
+
+});
+
 
